3932614c9603c66bb6769b81343858feba42df61
Victor H. E. França <123619380+Victoreli71@users.noreply.github.com>
2024-11-13 21:52:04 -0300

    Create Aplicativo.py

---
+++ Aplicativo.py
@@ -0,0 +1,32 @@
+from fasthtml.common import fast_app, serve, Titled, RedirectResponse 
+from componentes import gerar_titulo, gerar_formulario, gerar_lista_tarefas
+
+app, routes = fast_app()
+
+lista_tarefa = []
+
+@routes("/")
+def homepage():
+    formulario = gerar_formulario()
+    elemento_lista_tarefas = gerar_lista_tarefas(lista_tarefa)
+    return Titled("Lista de Tarefas", formulario, elemento_lista_tarefas)
+
+@routes("/adicionar_tarefa", methods=["post"])
+def adicionar_tarefa(tarefa: str):
+    if tarefa:
+        lista_tarefa.append(tarefa)
+    return gerar_lista_tarefas(lista_tarefa)
+
+@routes("/deletar/{posicao}")
+def deletar(posicao: int):
+    if len(lista_tarefa) > posicao:
+        lista_tarefa.pop(posicao)
+    return gerar_lista_tarefas(lista_tarefa)
+
+
+@routes("/blog")
+def homepage():
+    return gerar_titulo("blog ", "blog com artigos para você")
+
+
+serve()
